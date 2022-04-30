7d5511966c4f0e254b8f30422fe1c5c756bec340
Kiều Trọng Thiện <kieutrongthien@gmail.com>
2022-04-30 16:32:00 +0700

    UPdate

--- laravel-admin/resources/views/partials/footer.blade.php
+++ laravel-admin/resources/views/partials/footer.blade.php
@@ -14,5 +14,5 @@
 
     </div>
     <!-- Default to the left -->
-    <strong>Powered by <a href="https://github.com/z-song/laravel-admin" target="_blank">laravel-admin</a></strong>
+    <strong>Developed by <a href="https://omaicode.com" target="_blank">OMC</a></strong>
 </footer>
